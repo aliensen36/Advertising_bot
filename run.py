11a7a8e52c289018d5457fbbbff2a90ff7fa4e3b
Екатерина Александрова <aliensen65@gmail.com>
2024-09-23 12:00:00 +0300

    Изменения

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 import asyncio
 from dotenv import load_dotenv
 import os
 import logging
+from app.handlers import router
 
 load_dotenv()
 
@@ -11,8 +12,8 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
