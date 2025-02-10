f4f5818456de86baf653ad50f94cba28d3fc95e3
Екатерина Александрова <aliensen65@gmail.com>
2025-02-10 13:05:40 +0300

    Зависимости.

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 import asyncio
+from datetime import datetime
 import logging
+import sys
+import signal
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from dotenv import load_dotenv
+import os
 
-# Включаем логирование, чтобы не пропустить важные сообщения
+
+load_dotenv()
+
 logging.basicConfig(level=logging.INFO)
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
 # Объект бота
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+bot = Bot(BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
@@ -15,9 +25,40 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
+
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+    await dp.start_polling(bot, mylist=[1, 2, 3])
     await dp.start_polling(bot)
 
+# Обработка сигналов для корректного завершения работы бота
+def signal_handler(sig, frame):
+    logging.info("Получен сигнал завершения. Останавливаем бота...")
+    asyncio.create_task(shutdown())
+
+async def shutdown():
+    await bot.session.close()
+    sys.exit(0)
+
 if __name__ == "__main__":
+    # Регистрируем обработчик сигналов
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # Запускаем бота
     asyncio.run(main())
