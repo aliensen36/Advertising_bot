0981e1e43bc72c9faaebac31ab76c86278120824
Екатерина Александрова <aliensen65@gmail.com>
2025-02-10 14:18:30 +0300

    Изменения.

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from dotenv import load_dotenv
 import os
+from aiogram.client.default import DefaultBotProperties
+from aiogram import F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.enums import ParseMode
 
 
 load_dotenv()
@@ -16,9 +21,12 @@ logging.basicConfig(level=logging.INFO)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Объект бота
-bot = Bot(BOT_TOKEN)
+bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+# bot = Bot(BOT_TOKEN, parse_mode="HTML")
 # Диспетчер
 dp = Dispatcher()
+
+mylist = [1, 2, 3]
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
@@ -26,24 +34,18 @@ async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
 @dp.message(Command("add_to_list"))
-async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+async def cmd_add_to_list(message: types.Message):
     mylist.append(7)
     await message.answer("Добавлено число 7")
 
-
 @dp.message(Command("show_list"))
-async def cmd_show_list(message: types.Message, mylist: list[int]):
+async def cmd_show_list(message: types.Message):
     await message.answer(f"Ваш список: {mylist}")
 
 
 @dp.message(Command("info"))
 async def cmd_info(message: types.Message, started_at: str):
     await message.answer(f"Бот запущен {started_at}")
-
-from aiogram import F
-from aiogram.types import Message
-from aiogram.filters import Command
-from aiogram.enums import ParseMode
 
 # Если не указать фильтр F.text,
 # то хэндлер сработает даже на картинку с подписью /test
@@ -54,14 +56,20 @@ async def any_message(message: Message):
         parse_mode=ParseMode.HTML
     )
     await message.answer(
-        "Hello, *world*\!",
+        "Hello, *world*\\!",
         parse_mode=ParseMode.MARKDOWN_V2
+    )
+
+@dp.message(Command("hello"))
+async def cmd_hello(message: Message):
+    await message.answer(
+        f"Hello, <b>{message.from_user.full_name}</b>",
+        parse_mode=ParseMode.HTML
     )
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
-    await dp.start_polling(bot, mylist=[1, 2, 3])
     await dp.start_polling(bot)
 
 # Обработка сигналов для корректного завершения работы бота
