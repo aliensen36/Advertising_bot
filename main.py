afde2e16b9ad4e9e6f82dfd4a526d4abddfdcca3
Екатерина Александрова <aliensen65@gmail.com>
2025-02-10 19:46:42 +0300

    Изменения.

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
-import executor
+from aiogram import Bot, Dispatcher, F
+import asyncio
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 from dotenv import load_dotenv
 import os
-from aiogram import Bot, Dispatcher, types
-# from config import TOKEN_API
+import logging
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
-# Получаем токен бота
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply(f'Привет.\nТвой ID: {message.from_user.id}\nТвое имя: {message.from_user.full_name}')
+
+@dp.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer('Это команда /help')
+
+@dp.message(F.text == 'Как дела?')
+async def how_are_you(message: Message):
+    await message.answer('В полном порядке!')
+
+@dp.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo='https://static-cse.canva.com/blob/847132/paulskorupskas7KLaxLbSXAunsplash2.jpg',
+                               caption='Это фото')
 
 
-@dp.message_handler()
-async def echo_upper(message: types.Message):
-    await message.answer(message.text)
 
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот завершил работу')
 
